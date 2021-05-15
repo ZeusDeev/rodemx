0240d84478609ba0bef3871dbe4c26d43ffba156
zeus cach <83797099+devscode-cursos@users.noreply.github.com>
2021-05-14 20:32:26 -0500

    Add files via upload

---
+++ assets/js/formulario_index.js
@@ -0,0 +1,13 @@
+
+    function mostrar() {
+        Swal.fire(
+            'Tus datos fueron enviados correctamente',
+            'Nos pondremos en contacto contigo lo mas rápido posible y así comunicarnos',
+            'success'
+          )
+    }
+
+
+
+
+
